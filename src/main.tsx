073f496c52586d47b03cc4a65d828dc7aebaa535
lohitkumar1914 <lohitkumar1914@gmail.com>
2024-10-08 15:23:06 +0530

    dark mode implimented

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
+import theme from './theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+      <App />
     </ChakraProvider>
-  </StrictMode>,
+  </StrictMode>, 
 )
